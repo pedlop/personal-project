3992418453c565fb8f83790218362151ae96ca37
Pedro Victor <pedrolopes09.pv@gmail.com>
2017-09-01 17:01:13 -0300

    Adicionando o subscribe para o Login e a logica do Reactive Forms

--- tarefas-app/src/app/shared/autenticar/entrar/entrar.component.ts
+++ tarefas-app/src/app/shared/autenticar/entrar/entrar.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import { Router } from '@angular/router';
+
+import { AutenticarService } from './../shared/autenticar.service';
+import { ToastService } from './../../toast/toast.service';
 
 @Component({
   selector: 'ufg-entrar',
@@ -7,21 +12,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EntrarComponent implements OnInit {
 
-  usuario: any = {};
+  entrarForm: FormGroup;
+  loading: boolean;
 
-  constructor() {
-    this.usuario = {
-      nome: null,
-      email: null
-    }
-  }
+  constructor(private formBuilder: FormBuilder, private autenticarService: AutenticarService,
+              private router: Router, private toast: ToastService) { }
 
   ngOnInit() {
+    this.entrarForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      senha: ['', Validators.required]
+    })
   }
 
-  onSubmit(form) {
-    console.log(form);
-    console.log(this.usuario);
+  onClickEntrar() {
+    this.mostrarLoading();
+    console.log(this.entrarForm.controls.username.value);
+    console.log(this.entrarForm.controls.senha.value);
+    this.autenticarService.login(
+      this.entrarForm.controls.username.value,
+      this.entrarForm.controls.senha.value
+    ).subscribe(
+      data => {
+        this.toast.sucesso('Sucesso', 'Você esta logado!');
+        this.router.navigate(['/']);
+      },
+      err => {
+        this.toast.erro('', err);
+      }
+    )
     
+    
+  }
+
+  private mostrarLoading() {
+    this.loading = true;
+  }
+
+  private esconderLoading() {
+    this.loading = false;
   }
 }
