a4f61f99c023687ec1e45b9a4c121c13e5c9e961
Pedro Victor <pedrolopes09.pv@gmail.com>
2017-09-01 16:59:10 -0300

    Carregando dados de usuário logado e emitindo alerta pelo Toast

--- tarefas-app/src/app/pagina-inicial/pagina-inicial.component.ts
+++ tarefas-app/src/app/pagina-inicial/pagina-inicial.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Usuario } from './../shared/perfil-usuario/shared/usuario.model';
+import { PerfilUsuarioService } from './../shared/perfil-usuario/shared/perfil-usuario.service';
+import { ToastService } from './../shared/toast/toast.service';
+import { AutenticarService } from './../shared/autenticar/shared/autenticar.service';
+
 @Component({
   selector: 'ufg-pagina-inicial',
   templateUrl: './pagina-inicial.component.html',
@@ -7,9 +12,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PaginaInicialComponent implements OnInit {
 
-  constructor() { }
+  usuarioAtual: Usuario;
+  usuarios: any[];
+
+  constructor(private perfilUsuarioService: PerfilUsuarioService, private toast: ToastService,
+              private autenticarService: AutenticarService) {
+    this.usuarios = [];
+    this.usuarioAtual = JSON.parse(localStorage.getItem('usuarioAtual'));
+  }
 
   ngOnInit() {
+    this.carregarTodosUsuarios();
+  }
+
+  deletarUsuario(id: number) {
+    this.perfilUsuarioService.deletar(id).subscribe(
+      () => {
+        this.toast.sucesso('Sucesso!', 'Usuário excluído.');
+        this.carregarTodosUsuarios();
+      }
+    )
+  }
+
+  logout() {
+    this.autenticarService.logout();
+    this.toast.info('', 'Até logo!');
+  }
+
+  private carregarTodosUsuarios() {
+    this.perfilUsuarioService.getTodosUsuarios()
+    .subscribe(data => { this.usuarios = data; });
   }
 
 }
