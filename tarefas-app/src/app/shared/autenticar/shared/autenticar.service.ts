9d4a98a49ae555f7b958d6271bd3e29f77d843c5
Pedro Victor <pedrolopes09.pv@gmail.com>
2017-09-01 17:02:53 -0300

    Service para realizar o login e o logout

--- tarefas-app/src/app/shared/autenticar/shared/autenticar.service.ts
+++ tarefas-app/src/app/shared/autenticar/shared/autenticar.service.ts
@@ -1,8 +1,35 @@
 import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { Router } from '@angular/router';
+import { Observable } from "rxjs/Observable";
+import 'rxjs/add/operator/map'
 
 @Injectable()
 export class AutenticarService {
 
-  constructor() { }
+  constructor(private http: Http, private router: Router) { }
 
+  login(username: string, senha: string) {
+    return this.http.post('/api/autenticar', 
+    JSON.stringify(
+      {
+        username: username,
+        senha: senha
+      }
+    )).map((response: Response) => {
+      // login bem-sucedido se houver um token jwt na resposta
+      let usuario = response.json();
+      if (usuario && usuario.token) {
+        localStorage.setItem('usuarioAtual', JSON.stringify(usuario));
+      }
+      // armazene detalhes do usuário e token jwt no 'local storage' para manter o usuário logado entre as atualizações de página
+      return usuario;
+    });
+  }
+
+  logout() {
+    // remove usuario do 'local storage' para ele sair da aplicação
+    localStorage.removeItem('usuarioAtual');
+    this.router.navigate(['/login']);
+  }
 }
