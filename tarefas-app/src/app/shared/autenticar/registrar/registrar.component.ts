04ea60f2f68e2925f7342008125e54131b8eb279
Pedro Victor <pedrolopes09.pv@gmail.com>
2017-09-01 17:01:57 -0300

    Adicionando o subscribe para o Login e a logica do Reactive Forms

--- tarefas-app/src/app/shared/autenticar/registrar/registrar.component.ts
+++ tarefas-app/src/app/shared/autenticar/registrar/registrar.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+import { PerfilUsuarioService } from './../../perfil-usuario/shared/perfil-usuario.service';
+import { ToastService } from './../../toast/toast.service';
 
 @Component({
   selector: 'ufg-registrar',
@@ -7,9 +11,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrarComponent implements OnInit {
 
-  constructor() { }
+  registrarForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder, private perfilUsuarioService: PerfilUsuarioService,
+              private toastService: ToastService) { }
 
   ngOnInit() {
+    this.registrarForm = this.formBuilder.group({
+      nome: ['', Validators.required],
+      sobrenome: ['', Validators.required],
+      username: ['', Validators.required],
+      senha: ['', Validators.compose([Validators.required, Validators.minLength(3)])]
+    });
+  }
+
+  onClickRegistrar() {
+    this.perfilUsuarioService.criar(
+      this.registrarForm.controls.nome.value,
+      this.registrarForm.controls.sobrenome.value,
+      this.registrarForm.controls.username.value,
+      this.registrarForm.controls.senha.value
+    ).subscribe(
+      data => {
+        this.toastService.sucesso('Sucesso', 'Você foi registrado');
+      },
+      err => {
+        this.toastService.erro('', err);
+        console.log(err);
+      }
+    );
   }
 
 }
