e2a9675ecb228d3c609cadb46e25656022b0e13a
Pedro Victor <pedrolopes09.pv@gmail.com>
2017-09-01 15:50:16 -0300

    Importações do Mock, Toast e PerfilUsuario Module

--- tarefas-app/src/app/app.module.ts
+++ tarefas-app/src/app/app.module.ts
@@ -2,14 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
 
+// usado para criar o fake backend
+import { fakeUserBackendProvider } from './../_server/index';
+import { MockBackend, MockConnection } from '@angular/http/testing';
+import { BaseRequestOptions } from '@angular/http';
+
 import { AppComponent } from './app.component';
 import { PaginaInicialComponent } from './pagina-inicial/pagina-inicial.component';
 import { PaginaNaoEncontradaComponent } from './pagina-nao-encontrada/pagina-nao-encontrada.component';
 
+import { ToastModule } from './shared/toast/toast.module';
 import { HeaderModule } from './shared/header/header.module';
 import { FooterModule } from './shared/footer/footer.module';
 import { TarefaModule } from './tarefas/tarefa.module';
 import { AutenticarModule } from './shared/autenticar/autenticar.module';
+import { PerfilUsuarioModule } from './shared/perfil-usuario/perfil-usuario.module';
 import { AppRoutingModule } from './app-routing.module';
 
 
@@ -22,13 +29,20 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [
     BrowserModule,
     HttpModule,
+    ToastModule,
     HeaderModule,
     FooterModule,
     TarefaModule,
     AutenticarModule,
+    PerfilUsuarioModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    // 'providers' usados para criar o 'fake backend'
+    fakeUserBackendProvider,
+    MockBackend,
+    BaseRequestOptions
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
