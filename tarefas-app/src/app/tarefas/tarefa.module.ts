f8c7c5406ebd049a149f24da483b11a75ca8013b
Pedro Victor <pedrolopes09.pv@gmail.com>
2017-09-03 01:40:16 -0300

    Importando Modal, Reactive e Animations

--- tarefas-app/src/app/tarefas/tarefa.module.ts
+++ tarefas-app/src/app/tarefas/tarefa.module.ts
@@ -1,16 +1,24 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
-import { TarefaComponent } from './tarefa/tarefa.component';
+import { ModalModule } from 'ngx-bootstrap/modal';
+
 import { TarefaCadastrarComponent } from './tarefa-cadastrar/tarefa-cadastrar.component';
 import { TarefaRoutingModule } from './tarefa-routing.module';
 import { TarefaService } from './shared/tarefa.service';
 import { AutenticarGuard } from './../shared/autenticar/shared/autenticar.guard';
+import { TarefaAlterarComponent } from './tarefa-alterar/tarefa-alterar.component';
 
 @NgModule({
-    declarations: [TarefaComponent, TarefaCadastrarComponent],
-    imports: [CommonModule, TarefaRoutingModule],
-    exports: [TarefaComponent, TarefaCadastrarComponent],
+    declarations: [TarefaCadastrarComponent, TarefaAlterarComponent],
+    imports: [
+        CommonModule, 
+        TarefaRoutingModule,
+        ReactiveFormsModule,
+        ModalModule.forRoot()
+    ],
+    exports: [TarefaCadastrarComponent],
     providers: [TarefaService, AutenticarGuard]
 })
 export class TarefaModule { }
