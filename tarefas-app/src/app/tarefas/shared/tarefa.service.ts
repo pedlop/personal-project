f92e0e936a2172c76f58a3248dd697eaf1549afb
Pedro Victor <pedrolopes09.pv@gmail.com>
2017-09-02 23:26:44 -0300

    Adicionado o serviço para as tarefas
    Passando todos os métodos http para comunicação com nosso fake-bakend.

--- tarefas-app/src/app/tarefas/shared/tarefa.service.ts
+++ tarefas-app/src/app/tarefas/shared/tarefa.service.ts
@@ -1,8 +1,30 @@
 import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+
+import { Tarefa } from './tarefa.model';
 
 @Injectable()
 export class TarefaService {
 
-  constructor() { }
+  constructor(private http: Http) { }
 
+  mostrarTarefas() {
+    return this.http.get('/api/tarefas')
+    .map((response: Response) => response.json());
+  }
+
+  cadastrar(tarefa: Tarefa) {
+    return this.http.post('/api/tarefas', tarefa)
+    .map((response: Response) => response.json());
+  }
+  
+  alterar(titulo: string, descricao: string, id: number) {
+    return this.http.put('/api/tarefas/' + id, {titulo: titulo, descricao: descricao})
+    .map((response: Response) => response.json());
+  }
+
+  deletar(id: number) {
+    return this.http.delete('/api/tarefas/' + id)
+    .map((response: Response) => response.json());
+  }
 }
